6d34a354173915ddb4d262076341e2fef98ec3de
Vanessa <vanessa.legand@gmail.com>
2020-09-30 14:35:59 +0200

    Merge branch 'origin/P6_etape2'

--- js/case.js
+++ js/case.js
@@ -1,54 +1,76 @@
 class Case extends Composant {
-    constructor(id, colonne, rangee, domTarget) {
-        super(id,domTarget, "case");        
-        this.DOM.innerText = id;
-        this.colonne = colonne;
-        this.rangee = rangee;
-        this.obstacles = false;
-        this.joueurs = null;
-        this.arme = null;
-        this.casePossible = null;
-        window.jeu.cases[id] = this;
-    }
+  constructor(id, colonne, rangee, domTarget) {
+    super(id, domTarget, "case");
+    this.DOM.innerText = id;
+    this.colonne = colonne;
+    this.rangee = rangee;
+    this.obstacles = false;
+    this.joueur = null;
+    this.arme = null;
+    this.casePossible = null;
+    window.jeu.cases[id] = this;
+  }
 
-    majObstacles() {
-        if (this.obstacles) return false;
-        this.obstacles = true;
-        this.render();
-        return true;
-    }
+  majObstacles() {
+    if (this.obstacles) return false;
+    this.obstacles = true;
+    this.render();
+    return true;
+  }
 
-    majJoueurs(id) {
-        if (this.joueurs !== null || this.obstacles) return false;
-        this.joueurs = id;
-        this.render();
-        return true;
-    }
+  majJoueur(id) {
+    if (this.joueur !== null || this.obstacles) return false;
+    this.joueur = id;
+    this.render();
+    return true;
+  }
 
-    majArmes(arme) {
-      if (this.arme !== null || this.obstacles || this.joueurs) return false;
-      this.arme = arme;
-      this.render();
-      return true;
-    }
+  majArmes(arme) {
+    if (this.arme !== null || this.obstacles || this.joueur) return false;
+    this.arme = arme;
+    this.render();
+    return true;
+  }
 
-    majCasePossible() {
-        if (this.joueurs !== null || this.obstacles) return false;
-        this.casePossible = true;
-        this.render();
-        return true;
+  majCasePossible(affiche=true) {
+    if (!affiche){
+      this.casePossible = false;
+      this.render;
+      return;
     }
+    if (this.joueur !== null || this.obstacles) return false;
+    this.casePossible = true;
+    this.render();
+    return true;
+  }
 
-    render() {
-      this.DOM.style.backgroundImage = "";
-        if (this.obstacles) return this.DOM.className = "obstacle";
-        if (this.arme) {
-          this.DOM.className = "arme";
-          this.DOM.style.backgroundImage = `url(img/${this.arme.image})`;
-        }
-        if (this.joueurs !== null) return this.DOM.className = "joueur" + this.joueurs;
-        if (this.casePossible !== null) return this.DOM.className += " casePossible";
+  render() {
+    this.DOM.style.backgroundImage = "";
+    this.DOM.onclick = null;
+    if (this.obstacles) return this.DOM.className = "obstacle";
+    if (this.arme) {
+      this.DOM.className = "arme";
+      this.DOM.style.backgroundImage = `url(img/${this.arme.image})`;
     }
+    if (this.joueur !== null) return this.DOM.className = "joueur" + this.joueur;
+    if (this.casePossible !== null) {
+      this.DOM.className += " casePossible";
+      this.DOM.onclick = this.click.bind(this);
+    }
+  }
+  click(){
+    const joueurActuel = jeu.joueurActuel;
+    jeu.joueurs[joueurActuel].seDeplace(this.nom);
+    this.joueur = joueurActuel;
+    this.render();
+  }
+
+  echangeArme(armeJoueur){
+    if (this.arme === null) return null;
+    const armeSurLaCase = this.arme;
+    this.arme = armeJoueur;
+    return armeSurLaCase;
+  }
 }
 
 
@@ -79,8 +101,8 @@ class Composant{
     constructor(domTarget, id, nom, point){
       this.DOM = document.createElement(class);
       domTarget.appendChild("joueur");
-      window.jeu[this.name] = this;  
-      this.id = id;    
+      window.jeu[this.name] = this;
+      this.id = id;
       this.nom = nom;
       this.point = point;
     }
--- js/joueur.js
+++ js/joueur.js
@@ -8,6 +8,7 @@ class Joueur extends Composant{
     this.id = id;
     this.points = 100;
     this.position = position;
+    this.deplacements;
     document.documentElement.style.setProperty(`--joueur${this.id}Image`, ` center / contain no-repeat url("../img/${this.nom}.png")`);
     jeu.joueurs.push(this);
     this.render();
@@ -20,6 +21,45 @@ class Joueur extends Composant{
     <ligne>Points de vie : ${this.points}</ligne>
     <ligne>${this.arme.nomArme}</ligne>
     <ligne>Dégats : ${this.arme.dommages}</ligne>
-    `
+    `;
+  }
+
+  joue(){
+    this.deplacements = jeu.casePossible(this.position,3,false);
+    this.afficheMouvements("bas");
+    this.afficheMouvements("droite");
+    this.afficheMouvements("gauche");
+    this.afficheMouvements("haut");
+  }
+
+  /**
+   * [afficheMouvements description]
+   *
+   * @param   {string}  direction  bas ou droite ou gauche ou haut
+   *
+   * @return  {void}
+   */
+  afficheMouvements(direction){
+    for (let i=0, casesPossibles= this.deplacements[direction].length; i<casesPossibles; i++){
+      if(! jeu.cases[this.deplacements[direction][i]].majCasePossible()) return;
+    }
+  }
+
+  masqueMouvements(direction){
+    for (let i=0, casesPossibles= this.deplacements[direction].length; i<casesPossibles; i++){
+      jeu.cases[this.deplacements[direction][i]].majCasePossible(false);
+    }
+  }
+
+  seDeplace(idCase){
+    console.log(idCase);
+    this.position = idCase;
+    this.masqueMouvements("bas");
+    this.masqueMouvements("droite");
+    this.masqueMouvements("gauche");
+    this.masqueMouvements("haut");
+    
+    const nouvelleArme = jeu.cases[idCase].echangeArme(this.arme);
+    if (nouvelleArme !== null) this.arme = nouvelleArme;
   }
 }
